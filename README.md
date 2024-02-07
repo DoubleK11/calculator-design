701f027d26856267b94c9e266e4fbdd204dc0843
DoubleK11 <kevinkhoza8@gmail.com>
2024-02-07 13:39:26 +0200

    my design

---
+++ README.md
@@ -0,0 +1,45 @@
+#CALCULATOR DESIGN 
+
+I made this concept design on Figma, and with
+this mini repo project i will try to replicate it.
+
+##Day one: 21 January 2014
+
+I worked on how the calcultor's visible features
+will look. I workd with CSS and HTML to make it
+look at leats convinceing....
+
+Challenges: I still have a tought time with making
+most my desings responsive, and the artistic
+side of things..
+
+The bright Side: I managed to actually motivated myself
+to use Figma and code something, with almost a sure idea
+of what im doing.
+
+
+##Day Three: 23 January 2024
+
+I have learned how to manipulate the DOM a little bit, 
+I still have a lot to learn  but I am happy with the 
+progress I am maiking.
+
+Challenges: I have a tough time  with making sense of how
+things really work, and it's hard to beieve that a small
+project like this would require such a huge client script file.
+
+##Day Four: 24 January 2024
+
+I lost the loop of Documenting my progress, and have been working for 
+a long ass day, I am going to continue later.. I need know, to figure
+out how will I make this function like a real calclator
+
+###Day Five: 25 Janaury 2024.
+
+I took a break and did not do anythig with regard to this programme.
+
+###Day Six: 25 January 2024.
+
+I still can't figure out how to make the calculator function, like
+an actual calculator.
+After soo may attemps and failures all i can say is I'm tired.
---
+++ script.js
@@ -0,0 +1,211 @@
+//declaration and initialization of constants
+const inputScreen = document.getElementById("output-value");
+const resultOfOperation = document.getElementById("resultant");
+const clearButton = document.getElementById("clear");
+const squareRootButton = document.getElementById("squareRoot");
+const percentageSign = document.getElementById("percentage");
+const powerButtonOnOff = document.getElementById("onOff");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+const minus = document.getElementById("minus");
+const equal = document.getElementById("equal");
+const plus = document.getElementById("plus");
+const division = document.getElementById("division");
+const multiply = document.getElementById("multiply");
+
+
+// End of contants keys
+
+
+//object for keybutton values
+
+/*   
+   This is aryy and objects is meant to have information i'll use for configuring 
+   button operations.  
+*/
+
+const arrayOfObjectKeys = [
+     //fnctional buttons
+    {
+        name: clearButton,
+        "button value": "clear",
+        "button id": "clear",
+       "button function": clearFunct,
+        "button text": "clear",
+    },
+    {
+        name : squareRootButton,
+        "button value": "square Root operator",
+        "button id": "squareRoot",
+    //    "button function": squareRootFunct,
+        "button text": "&sqrt;",
+    },
+    {
+        name: percentageSign,
+        "button value": "percantage operator",
+        "button id": "percentage",
+      //  "button function": percentageFunct,
+        "button text": "%"
+    },
+    {
+        name: powerButtonOnOff,
+        "button value": "On/Off",
+        "button id": "onOff",
+       "button function": powerBtnFunc,
+        "button text": "On/Off",
+    },
+    //bellow is the four index
+    //for the numbers
+
+
+    {
+        name: one,
+        "button value": 1,
+        "button id": "one",
+        "button text": "1",
+    },
+    {
+        name: two,
+        "button value": 2,
+        "button id": "two",
+        "button text": "2",
+    },
+    {
+        name: three,
+        "button value": 3,
+        "button id": "three",
+        "button text": "3",
+    },
+    {
+        name: four,
+        "button value": 4,
+        "button id": "four",
+        "button text": "4",
+    },
+    {
+        name: five,
+        "button value": 5,
+        "button id": "five",
+        "button text": "5",
+    },
+    {
+        name: six,
+        "button value": 6,
+        "button id": "six",
+        "button text": "6",
+    },
+    {
+        name: seven,
+        "button value": 7,
+        "button id": "seven",
+        "button text": "7",
+    },
+    {
+        name: eight,
+        "button value": 8,
+        "button id": "eight",
+        "button text": "8",
+    },
+    {
+        name: nine,
+        "button value": 9,
+        "button id": "nine",
+        "button text": "9",
+    },
+    {
+        name: zero,
+        "button value": 0,
+        "button id": "zero",
+        "button text": "0",
+    },
+
+    //arithmetic operators
+    {
+        name : minus,
+        "button value": "-",
+        "button id": "minust",
+   //     "button function": minusButtonFunc,
+        "button text": "-",
+    },
+    {
+        name : equal,
+        "button value": "=",
+        "button id": "equal",
+   //     "button function": equalButtonFunc,
+        "button text": "&sqrt;",
+    },
+    {
+        name : plus,
+        "button value": "+",
+        "button id": "plus",
+    //    "button function": plusButtonFunc,
+        "button text": "+;",
+    },
+    {
+        name : division,
+        "button value": "/",
+        "button id": "division",
+      //  "button function": divisionButtonFunc,
+        "button text": "/",
+    },
+    {
+        name : multiply,
+        "button value": "*",
+        "button id": "multiply",
+   //     "button function": multiplyButtonFunc,
+        "button text": "*",
+    },
+];
+
+//special function buttons
+
+powerButtonOnOff.addEventListener("click", powerBtnFunc)
+
+function powerBtnFunc() {
+    
+    if(inputScreen.style.visibility === "hidden") {
+        inputScreen.style.visibility = "visible";
+    } else {
+        inputScreen.style.visibility = "hidden";
+    }
+    console.log(arrayOfObjectKeys[3]["button text"])
+    clearFunct()
+}
+
+function rejectOperation() {
+        if(inputScreen.style.visibility === "hidden") {
+            inputScreen.innerText = 0;
+        }
+
+}
+
+clearButton.addEventListener("click",clearFunct)
+
+function clearFunct() {
+    inputScreen.innerText = arrayOfObjectKeys[13]["button text"];
+}
+
+//end of function buuttons
+
+//numerals
+
+one.addEventListener("click", ()=> {
+    inputScreen.innerText += arrayOfObjectKeys[4]["button value"];
+    rejectOperation();
+})
+two.addEventListener("click", ()=>{
+    inputScreen.innerText += arrayOfObjectKeys[5]["button value"];
+    rejectOperation();
+})
+three.addEventListener("click", ()=>{
+    inputScreen.innerText += arrayOfObjectKeys[6]["button value"];
+    rejectOperation()
+})
